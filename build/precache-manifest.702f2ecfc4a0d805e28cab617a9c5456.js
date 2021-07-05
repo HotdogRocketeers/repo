8f3994a1e844c5333469c2b74c0497ca7c8d18a9
HotdogRocketeers <86943417+HotdogRocketeers@users.noreply.github.com>
2021-07-05 21:16:34 +0800

    Add files via upload

---
+++ build/precache-manifest.702f2ecfc4a0d805e28cab617a9c5456.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "e60650cbed916c075b6f",
+    "url": "/static/css/main.f5f1612f.chunk.css"
+  },
+  {
+    "revision": "e60650cbed916c075b6f",
+    "url": "/static/js/main.a74a2441.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8d33b00f5dc685059e84",
+    "url": "/static/css/2.8535999b.chunk.css"
+  },
+  {
+    "revision": "8d33b00f5dc685059e84",
+    "url": "/static/js/2.188f9a18.chunk.js"
+  },
+  {
+    "revision": "26cce1a2bead7595e61ec5733e4d75e8",
+    "url": "/static/media/HotdogRocketWhite.26cce1a2.png"
+  },
+  {
+    "revision": "d15b2b2356986eba9bf0e97d31a9d106",
+    "url": "/index.html"
+  }
+];
